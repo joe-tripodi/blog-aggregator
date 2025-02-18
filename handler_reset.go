0b1ba4a15211a19f7b933eee0eeca31c5fe0d8e2
joe-tripodi <joe@flowofcode.com>
2025-02-18 14:58:57 +1030

    add reset

---
+++ handler_reset.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func handlerReset(s *state, cmd command) error {
+	err := s.db.DeleteUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("failed to reset db: %w", err)
+	}
+	fmt.Println("successfully reset the db")
+	return nil
+}
